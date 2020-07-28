ac4ac368dfa3a65bdc3d8dd901f278135ea6c548
nikito7 <45373783+nikito7@users.noreply.github.com>
2020-07-28 03:04:16 +0100

    Update bw2mqtt.sh

--- bw2mqtt.sh
+++ bw2mqtt.sh
@@ -17,14 +17,14 @@ for n in rx tx
 do
 mosquitto_pub -t "homeassistant/sensor/$id/${dev}_${n}/config" \
 -m "{\"unit_of_measurement\":\"kB/s\",\
- \"icon\":\"mdi:arrow\",\
- \"name\":\"$id $dev $n\",\
+ \"icon\":\"mdi:time\",\
+ \"name\":\"$name $dev $n\",\
  \"state_topic\":\"$topic/$id/$dev/$n\",\
  \"availability_topic\":\"$topic/$id/status\",\
  \"unique_id\":\"$id-$dev-$n\",\
  \"device\":{\
  \"identifiers\":\"$id\",\
- \"name\":\"$id\",\
+ \"name\":\"$name\",\
  \"sw_version\":\"v0\",\
  \"model\":\"x\",\
  \"manufacturer\":\"x\"}}"
@@ -35,38 +35,35 @@ done
 
 function stats()
 {
-getbytes="" && getbytes=$(ifconfig $1 | grep bytes: | sed 's/:/\ /g')
+getbytes=""; getbytes=$(ifconfig $1 | grep bytes: | sed 's/:/\ /g')
 rxaa=$(echo "$getbytes" | awk '{ print $3 }')
 txaa=$(echo "$getbytes" | awk '{ print $8 }')
 
 sleep 3
 
-getbytes="" && getbytes=$(ifconfig $1 | grep bytes: | sed 's/:/\ /g')
+getbytes=""; getbytes=$(ifconfig $1 | grep bytes: | sed 's/:/\ /g')
 rxbb=$(echo "$getbytes" | awk '{ print $3 }')
 txbb=$(echo "$getbytes" | awk '{ print $8 }')
 
-result_rx="" && result_rx=$( expr $(expr $rxbb - $rxaa) / 1024 / 3 )
-result_tx="" && result_tx=$( expr $(expr $txbb - $txaa) / 1024 / 3 )
+result_rx=""; result_rx=$( expr $(expr $rxbb - $rxaa) / 1024 / 3 )
+result_tx=""; result_tx=$( expr $(expr $txbb - $txaa) / 1024 / 3 )
 
 echo RX $result_rx
 echo TX $result_tx
 
 devx=$(echo $1 | sed 's/\./_/g')
 
-mosquitto_pub -t $topic/$id/status -m online
 mosquitto_pub -t $topic/$id/$devx/rx -m $result_rx
 mosquitto_pub -t $topic/$id/$devx/tx -m $result_tx
 
 }
 
-while [ 2 -gt 1 ]
-do
 for dev in $devlist
 do
 stats $dev
 done
-sleep 7
-done
+
+sleep 9 && /bin/sh $0 &
 
 ### bw2mqtt.sh ###
 ##
