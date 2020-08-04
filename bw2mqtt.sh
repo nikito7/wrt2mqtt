7107ea3206644cdf4f79eb15afe2b5ace2d0ab2b
nikito7 <45373783+nikito7@users.noreply.github.com>
2020-08-04 17:14:27 +0100

    Update bw2mqtt.sh

--- bw2mqtt.sh
+++ bw2mqtt.sh
@@ -2,9 +2,9 @@
 
 ### bw2mqtt.sh ###
 
-name="RT1"
-id=rt_101_v1
-devlist="eth0.2 eth5 eth7"
+name="Router 1"
+id=rt_1_v2
+devlist="eth0.2 eth5"
 topic=wrt2mqtt
 mqttpub="mosquitto_pub"
 interval=2
@@ -18,16 +18,16 @@ icon=$2
 dev=$4
 devx=$(echo $4 | sed 's/\./_/g')
 #
-$mqttpub -t "homeassistant/sensor/$id/${devx}_${1}/config" \
+$mqttpub -t "homeassistant/sensor/${id}/${devx}_${1}/config" \
 -m '{
  "unit_of_measurement":"kB/s",
  "icon":"'$icon'",
  "name":"'"$name $3 $dev"'",
- "state_topic":"'"$topic/$id/${devx}_${1}"'",
- "availability_topic":"'$topic/$id/status'",
+ "state_topic":"'"$topic/${id}/${devx}_${1}"'",
+ "availability_topic":"'$topic/${id}/status'",
  "unique_id":"'"${id}_${devx}_$1"'",
  "device":{
-   "identifiers":"'$id'",
+   "identifiers":"'${id}'",
    "name":"'"$name"'",
    "model":"'"$model"'"}
  }'
@@ -36,29 +36,29 @@ $mqttpub -t "homeassistant/sensor/$id/${devx}_${1}/config" \
 
 ###
 
-if [ ! -e /tmp/bw2mqtt.config.$id ]
+if [ ! -e /tmp/bw2mqtt.config.${id} ]
 then
 for dev in $devlist
 do
 home rx mdi:arrow-down RX $dev
 home tx mdi:arrow-up TX $dev
 done
-echo $id > /tmp/bw2mqtt.config.$id
+echo ${id} > /tmp/bw2mqtt.config.${id}
 fi
 
 ###
 
-$mqttpub -t "homeassistant/binary_sensor/$id/${id}_status/config" \
+$mqttpub -t "homeassistant/binary_sensor/${id}/${id}_status/config" \
 -m '{
   "device_class":"connectivity",
   "payload_on":"online",
   "payload_off":"offline",
   "name":"'"$name Status"'",
-  "state_topic":"'"$topic/$id/status"'",
-  "availability_topic":"'$topic/$id/status'",
+  "state_topic":"'"$topic/${id}/status"'",
+  "availability_topic":"'$topic/${id}/status'",
   "unique_id":"'"${id}_status"'",
   "device":{
-    "identifiers":"'$id'",
+    "identifiers":"'${id}'",
     "name":"'"$name"'",
     "model":"'"$model"'"}
   }'
@@ -85,9 +85,9 @@ echo TX $result_tx
 
 devx=$(echo $1 | sed 's/\./_/g')
 
-$mqttpub -t $topic/$id/status -m online
-$mqttpub -t $topic/$id/${devx}_rx -m $result_rx
-$mqttpub -t $topic/$id/${devx}_tx -m $result_tx
+$mqttpub -t $topic/${id}/status -m online
+$mqttpub -t $topic/${id}/${devx}_rx -m $result_rx
+$mqttpub -t $topic/${id}/${devx}_tx -m $result_tx
 
 }
 
